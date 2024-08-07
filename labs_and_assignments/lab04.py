e4f6305656a22c342282890f09ec0a6b505b69f6
Leon <pleon@email.neit.edu>
2024-08-07 09:48:22 -0400

    week4 lab finished, lab4 started

--- labs_and_assignments/lab04.py
+++ labs_and_assignments/lab04.py
@@ -10,6 +10,24 @@
 #---Imports---------------
 import csv
 
-#---Functions-------------
-
 #---Main Code-------------
+
+#initialize parallel lists
+nameFirst = []
+nameLast = []
+age = []
+nickname = []
+house = []
+
+#connected to file----------------------------------------------
+with open("labs_and_assignments/lab4A_GOT_NEW.txt") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        nameFirst.append(rec[0])
+        nameLast.append(rec[1])
+        age.append(rec[2])
+        nickname.append(rec[3])
+        house.append(rec[4])
+        
+#disconnect----------------------------------------------------
