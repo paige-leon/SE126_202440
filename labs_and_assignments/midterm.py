d9af67d77c09f37fb0ae8d029c5a48dae113f7d6
Leon <pleon@email.neit.edu>
2024-08-12 11:20:54 -0400

    started messing around with a final project, idk what it is yet

---
+++ labs_and_assignments/midterm.py
@@ -0,0 +1,17 @@
+#Paige Leon
+#Midterm
+#Date 8/19/24
+#PROGRAM PROMPT:
+
+
+#Variable Dictionary
+#-----------------------------------
+
+#---Imports---------------
+import random
+
+#---Functions-------------
+
+
+#---Main Code-------------
+test = [1,2,3,4,5,6,7,8,9]
