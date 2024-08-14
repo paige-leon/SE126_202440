2d443f06fb44d2cd7e293b6eef4e6919b48885b2
Leon <pleon@email.neit.edu>
2024-08-14 10:14:19 -0400

    w5d2 notes

--- labs_and_assignments/midterm.py
+++ labs_and_assignments/midterm.py
@@ -9,11 +9,33 @@
 
 #---Imports---------------
 import random
+import csv
 
 #---Functions-------------
 
 
 #---Main Code-------------
+
+#variables
+indeck = []
+cname = []
+cvalue = []
+deck = []
+
+#open document
+with open("labs_and_assignments/cards.csv") as csvfile:
+    file = csv.reader(csvfile)
+
+    for rec in file:
+        indeck.append(1)
+        cname.append(rec[0])
+        cvalue.append(rec[1])
+        
+#Close document
+
+
+
+print(deck)
 test = [1,2,3,4,5,6,7,8,9]
 
 print(test)
