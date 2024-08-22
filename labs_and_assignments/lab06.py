db18b952c2205fd9873507cbf6350a01e60a8a62
Leon <pleon@email.neit.edu>
2024-08-22 09:05:52 -0400

    added notes for w6 and barely started assignment

---
+++ labs_and_assignments/lab06.py
@@ -0,0 +1,24 @@
+#Paige Leon
+#Lab #06
+#8/22/24
+#PROGRAM PROMPT:Write a program that can be used by a small theater to sell tickets for performances. The
+#theater’s auditorium has 15 rows of seats with 30 seats in each row. The program should display
+#a screen that shows which seats are available and which are taken. Seats thar are available are
+#represented by # and seats that are taken are represent by a *.
+#There are aisles after seats H and V.
+
+
+#Variable Dictionary
+#-----------------------------------
+
+#---Imports---------------
+
+
+#---Functions-------------
+def seatMap():
+    print("Put a map right here")
+    print("Row                    Seats")
+  
+
+#---Main Code-------------
+
