2280bcf87608fa83969217e321bc68f06496c69d
Paige Leon <paigeleon91@gmail.com>
2024-09-15 13:19:08 -0400

    Got the basic trick taking system working. Still need to add must follow for player and also card abilities and better visuals

--- labs_and_assignments/final.py
+++ labs_and_assignments/final.py
@@ -90,14 +90,13 @@ def leadTrick(leader):
     if leader == True:
         displayHand(playerHand)
         try:
-            choice = int(input("What card # would you like to play? "))
+            choice = int(input("What card # would you like to lead with? "))
 
             playerNum = playerHand[choice][0]
             playerSuit = playerHand[choice][1]
-            playerHand.pop(choice)
+            playerField = playerHand.pop(choice)
 
-            print("Testing new method of field", playerNum, playerSuit)
-            followTrick(choice)
+            return playerField
         except:
             input("\n\nERROR IN leadTrick(): Please enter a valid number from the list [press ENTER to continue] ")
             leadTrick()
@@ -108,32 +107,133 @@ def leadTrick(leader):
             ran = random.randint(0,len(aiHandD)-1)
             aiNum = aiHandD[ran][0]
             aiSuit = aiHandD[ran][1]
-            aiHandD.pop(ran)
+            aiField = aiHandD.pop(ran)
 
         elif suit == 2: #Shields
             ran = random.randint(0,len(aiHandS)-1)
             aiNum = aiHandS[ran][0]
             aiSuit = aiHandS[ran][1]
-            aiHandS.pop(ran)
+            aiField = aiHandS.pop(ran)
 
         elif suit == 3: #Feathers
             ran = random.randint(0,len(aiHandF)-1)
             aiNum = aiHandF[ran][0]
             aiSuit = aiHandF[ran][1]
-            aiHandF.pop(ran)
+            aiField = aiHandF.pop(ran)
         elif suit == 4: #Potions
             ran = random.randint(0,len(aiHandP)-1)
             aiNum = aiHandP[ran][0]
             aiSuit = aiHandP[ran][1]
-            aiHandP.pop(ran)
+            aiField = aiHandP.pop(ran)
 
-        print("ai field:",aiNum, aiSuit)
-        followTrick([aiNum, aiSuit], leader) #begin the trick with the card picked by the randomizer
+        return aiField        
 
 def followTrick(lead, leader):
-    print("followTrick(lead) = ", lead)
     if leader == True:
-        print("INSERT CPU AI HERE LOL")
+        
+        #Find the max and min for on/off suit play win/lose
+        max = 0
+        maxID = -1
+        min = 100
+        minID = -1
+        offSuit = False
+
+        #Leading Daggars
+        if lead[1] == "Daggers":
+
+            #AI Has on suit option
+            if len(aiHandD) != 0:
+                for i in range(0,len(aiHandD)):
+                    if aiHandD[i][0] > max:
+                        max = aiHandD[i][0]
+                        maxID = i
+                    if aiHandD[i][0] < min:
+                        min = aiHandD[i][0]
+                        minID = i
+                if max < lead[0]: #Return smallest on suit if can't win
+                    aiField = aiHandD.pop(minID)
+                else: #Return highest on suit if can win
+                    aiField = aiHandD.pop(maxID)
+            else:
+                offSuit = True
+
+        #Leading Shields
+        elif lead[1] == "Shields":
+            
+            #AI Has on suit option
+            if len(aiHandS) != 0:
+                for i in range(0,len(aiHandS)):
+                    if aiHandS[i][0] > max:
+                        max = aiHandS[i][0]
+                        maxID = i
+                    if aiHandS[i][0] < min:
+                        min = aiHandS[i][0]
+                        minID = i
+                if max < lead[0]: #Return smallest on suit if can't win
+                    aiField = aiHandS.pop(minID)
+                else: #Return highest on suit if can win
+                    aiField = aiHandS.pop(maxID)
+            else:
+                offSuit = True
+
+        #Leading Feathers
+        elif lead[1] == "Feathers":
+            
+            #AI Has on suit option
+            if len(aiHandF) != 0:
+                for i in range(0,len(aiHandF)):
+                    if aiHandF[i][0] > max:
+                        max = aiHandF[i][0]
+                        maxID = i
+                    if aiHandF[i][0] < min:
+                        min = aiHandF[i][0]
+                        minID = i
+                if max < lead[0]: #Return smallest on suit if can't win
+                    aiField = aiHandF.pop(minID)
+                else: #Return highest on suit if can win
+                    aiField = aiHandF.pop(maxID)
+            else:
+                offSuit = True
+
+        #Leading Potions       
+        elif lead[1] == "Potions":
+            #AI Has on suit option
+            if len(aiHandP) != 0:
+                for i in range(0,len(aiHandP)):
+                    if aiHandP[i][0] > max:
+                        max = aiHandP[i][0]
+                        maxID = i
+                    if aiHandP[i][0] < min:
+                        min = aiHandP[i][0]
+                        minID = i
+                if max < lead[0]: #Return smallest on suit if can't win
+                    aiField = aiHandP.pop(minID)
+                else: #Return highest on suit if can win
+                    aiField = aiHandP.pop(maxID)
+            else:
+                offSuit = True
+        else:
+            print(f"-ERROR- The suit {lead[1]} doesn't exist somehow??")
+        while offSuit == True:
+            suit = random.randint(0, 3)
+            if suit == 0 and len(aiHandD) != 0:
+                randHand = aiHandD
+            elif suit == 1 and len(aiHandS) != 0:
+                randHand = aiHandS
+            elif suit == 2 and len(aiHandF) != 0:
+                randHand = aiHandF
+            elif suit == 3 and len(aiHandP) != 0:
+                randHand = aiHandP
+            
+            #Look for a small one so it isn't wasted
+            min = 100
+            minID = -1
+            for i in range(0,len(randHand)):
+                if min > randHand[i][0]:
+                    min = randHand[i][0]
+                    minID = i
+            aiField = randHand.pop(minID)
+        return aiField
     else:
         displayHand(playerHand)
         print(f"\nCPU has played the {lead[0]} of {lead[1]}")
@@ -142,34 +242,44 @@ def followTrick(lead, leader):
 
             playerNum = playerHand[choice][0]
             playerSuit = playerHand[choice][1]
-            playerHand.pop(choice)
+            playerField = playerHand.pop(choice)
 
-            print("player", playerNum, playerSuit)
-            print("ai", aiNum, aiSuit)
         except:
             input("\n\nERROR IN followTrick: Please enter a valid number from the list [press ENTER to continue] ")
             followTrick(lead)
-        scoreTrick(lead, [playerNum,playerSuit], leader)
-
+        return playerField
 
 def scoreTrick(lead, follow, leader):
     '''Return true if leading player wins the trick.  Return false if leading player lost the trick'''
+    if leader == True:
+        print(f"You led with the {lead[0]} of {lead[1]}")
+        print(f"The AI followed with the {follow[0]} of {follow[1]}")
+    else:
+        print(f"The AI led with the {lead[0]} of {lead[1]}")
+        print(f"You followed with the {follow[0]} of {follow[1]}")
+
     if lead[1] == follow[1]: #following player played on suit
         if lead[0] >= follow[0]: #leading player had higher value
             if leader == True:
                 print("You won the trick!")
+                return True
             else:
                 print("You lost the trick! [lower number]")
-            return True 
+                return False
         else: #leading player had lower value
             if leader == True:
                 print("You lost the trick.. [lower number]")
+                return False
             else:
                 print("You won the trick!")
-            leader = -playerLeading #Set the leading player into the inverse of it's current state (AKA, the leading player won the trick so they are the new leading player)
-            return False
-    print("You lost the trick [played off suit]")
-    return True #follwing player played off suit
+                return True
+    else:
+        if leader == True:
+            print("You won the trick [Opponent played off suit]")
+            return True #ai played off suit
+        else:        
+            print("You lost the trick [played off suit]")
+            return False #player played off suit
 
 
 def daggers(lead, follow):
@@ -189,9 +299,11 @@ discard = []
 
 playerHand = [] #Players hand
 playerHealth = 0
+
+playerField = []
 playerNum = -1 #This is where the following card goes
 playerSuit = ""
-playerLeading = False
+playerLeading = True
 
 aiHand = []
 aiHandD = []
@@ -199,6 +311,8 @@ aiHandS = []
 aiHandF = []
 aiHandP = []
 aiHealth = 0
+
+aiField = []
 aiNum = -1 #This is where the card the ai used goes
 aiSuit = ""
 
@@ -217,15 +331,27 @@ for i in range(1, 9): #make a deck full of cards 1-8
 #Testing area :)))
 deck = decklist.copy()
 random.shuffle(deck)
+draw(10, playerHand)
+draw(10, aiHand)
+#newGame(20,10)
 
-newGame(20,10)
+#testing loop
+cnt = 0
 
-print(len(deck))
-draw(10,playerHand)
+while cnt <= 5:
 
-
-leadTrick(playerLeading)
-#displayHand(playerHand)
+    #AI leading Loop
+    if playerLeading == False:
+        aiField = leadTrick(playerLeading)
+        playerField = followTrick(aiField,playerLeading)
+        playerLeading = scoreTrick(aiField,playerField,playerLeading)
+    
+    #Player leading Loop
+    else:
+        playerField = leadTrick(playerLeading)
+        aiField = followTrick(playerField,playerLeading)
+        playerLeading = scoreTrick(playerField, aiField, playerLeading)
+    cnt += 1
 
 #Main Program Loop
 
